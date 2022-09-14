17dcf5ded573549f7401bd40f39bb1dae530dba3
Dmitry <Atlantiskun@gmail.com>
2022-09-14 13:33:39 +0700

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Дмитрий Болучевских on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
